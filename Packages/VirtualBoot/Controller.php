4fec57b0e1c0750c94cd5f4dbb690496053368c0
marcocastignoli <marco.castignoli@gmail.com>
2018-02-21 23:40:29 +0100

    Fix virtualboot controller

--- Packages/VirtualBoot/Controller.php
+++ Packages/VirtualBoot/Controller.php
@@ -10,36 +10,36 @@ class Controller extends App\Controller
         $package = $request->get('name');
         try {
             $this->VirtualBoot->install($package);
-            $this->Response->reply("Package " . $package . " installed");
+            $this->Output->reply("Package " . $package . " installed");
         } catch (\Exception $e) {
-            $this->Response->reply($e->getMessage());
+            $this->Output->reply($e->getMessage());
         }
     }
     public function uninstall($request){
         $package = $request->get('name');
         try {
             $this->VirtualBoot->uninstall($package);
-            $this->Response->reply("Package " . $package . " uninstalled");
+            $this->Output->reply("Package " . $package . " uninstalled");
         } catch (\Exception $e) {
-            $this->Response->reply($e->getMessage());
+            $this->Output->reply($e->getMessage());
         }
     }
     public function activate($request){
         $package = $request->get('name');
         try {
             $this->VirtualBoot->activate($package);
-            $this->Response->reply("Package " . $package . " activated");
+            $this->Output->reply("Package " . $package . " activated");
         } catch (\Exception $e) {
-            $this->Response->reply($e->getMessage());
+            $this->Output->reply($e->getMessage());
         }
     }
     public function deactivate($request){
         $package = $request->get('name');
         try {
             $this->VirtualBoot->deactivate($package);
-            $this->Response->reply("Package " . $package . " deactivated");
+            $this->Output->reply("Package " . $package . " deactivated");
         } catch (\Exception $e) {
-            $this->Response->reply($e->getMessage());
+            $this->Output->reply($e->getMessage());
         }
     }
 }
