c6c4aec0547da360d56e9c102024e6312ef23037
marcocastignoli <marco.castignoli@gmail.com>
2018-02-22 01:05:58 +0100

    Added dump component (output)

---
+++ Packages/Dump/Component.php
@@ -0,0 +1,20 @@
+<?php
+namespace Dump;
+
+use App;
+
+class Component
+{
+    public static $types = ['Output'];
+    
+    const CODE_SUCCESS = 0;
+    const CODE_MISSING_ROUTE = 1;
+    const CODE_MISSING_PARAMETER = 2;
+    const CODE_WRONG_DATABASE_CONFIGURATION = 3;
+    const CODE_WRONG_DATABASE_QUERY = 4;
+
+    public function reply($message, $code = self::CODE_SUCCESS)
+    {
+        var_dump($message);
+    }
+}
